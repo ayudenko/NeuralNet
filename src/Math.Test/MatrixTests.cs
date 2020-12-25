76ef81085ca4db017464f3ca35db89157ca72927
Andrey Yudenko <ayudenko@gmail.com>
2020-12-25 23:33:35 +0300

    Matrixes multiplication.

--- src/Math.Test/MatrixTests.cs
+++ src/Math.Test/MatrixTests.cs
@@ -49,12 +49,12 @@ namespace Math.Test
             Matrix result = matrix1.Sum(matrix2);
 
             // Assert
-            Assert.Equal(3, result.GetItem(0,0));
-            Assert.Equal(5, result.GetItem(0,1));
-            Assert.Equal(7, result.GetItem(1,0));
-            Assert.Equal(9, result.GetItem(1,1));
-            Assert.Equal(11, result.GetItem(2,0));
-            Assert.Equal(13, result.GetItem(2,1));
+            Assert.Equal(3, result.GetItem(0, 0));
+            Assert.Equal(5, result.GetItem(0, 1));
+            Assert.Equal(7, result.GetItem(1, 0));
+            Assert.Equal(9, result.GetItem(1, 1));
+            Assert.Equal(11, result.GetItem(2, 0));
+            Assert.Equal(13, result.GetItem(2, 1));
         }
 
         [Fact]
@@ -63,7 +63,7 @@ namespace Math.Test
             // Arrange
             Single[,] matrixItems1 = new Single[,] { { 1, 2 }, { 3, 4 }, { 5, 6 } };
             Matrix matrix1 = new(matrixItems1);
-            Single[,] matrixItems2 = new Single[,] { { 2, 3, 4}, { 5, 6, 7 } };
+            Single[,] matrixItems2 = new Single[,] { { 2, 3, 4 }, { 5, 6, 7 } };
             Matrix matrix2 = new(matrixItems2);
 
             Single[,] matrixItems3 = new Single[,] { { 1, 2 }, { 3, 4 }, { 5, 6 } };
@@ -112,18 +112,54 @@ namespace Math.Test
             // Arrange
             Single[,] matrixItems = new Single[,] { { 1, 1 }, { 1, 1 }, { 1, 1 } };
             Matrix matrix = new(matrixItems);
-            Single scalar = 2.5F;
+            Single scalar = 2.5f;
 
             // Act
             Matrix result = matrix.Multiply(scalar);
 
             // Assert
-            Assert.Equal(2.5F, result.GetItem(0, 0));
-            Assert.Equal(2.5F, result.GetItem(0, 1));
-            Assert.Equal(2.5F, result.GetItem(1, 0));
-            Assert.Equal(2.5F, result.GetItem(1, 1));
-            Assert.Equal(2.5F, result.GetItem(2, 0));
-            Assert.Equal(2.5F, result.GetItem(2, 1));
+            Assert.Equal(2.5f, result.GetItem(0, 0));
+            Assert.Equal(2.5f, result.GetItem(0, 1));
+            Assert.Equal(2.5f, result.GetItem(1, 0));
+            Assert.Equal(2.5f, result.GetItem(1, 1));
+            Assert.Equal(2.5f, result.GetItem(2, 0));
+            Assert.Equal(2.5f, result.GetItem(2, 1));
+        }
+
+        [Fact]
+        public void Multiply_PassMatrix_GetNewMatrix()
+        {
+            Single[,] matrixItems1 = new Single[,] { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+            Matrix matrix1 = new(matrixItems1);
+            Single[,] matrixItems2 = new Single[,] { { 1, 2, 3 }, { 4, 5, 6 } };
+            Matrix matrix2 = new(matrixItems2);
+
+            Matrix result1 = matrix1.Multiply(matrix2);
+
+            Assert.Equal(3, result1.GetDimension0());
+            Assert.Equal(3, result1.GetDimension1());
+            Assert.Equal(9f, result1.GetItem(0, 0));
+            Assert.Equal(12f, result1.GetItem(0, 1));
+            Assert.Equal(15f, result1.GetItem(0, 2));
+            Assert.Equal(19f, result1.GetItem(1, 0));
+            Assert.Equal(26f, result1.GetItem(1, 1));
+            Assert.Equal(33f, result1.GetItem(1, 2));
+            Assert.Equal(29f, result1.GetItem(2, 0));
+            Assert.Equal(40f, result1.GetItem(2, 1));
+            Assert.Equal(51f, result1.GetItem(2, 2));
+
+
+            Single[,] matrixItems3 = new Single[,] { { 3, 2, 1 }, { 0, 1, 2 } };
+            Matrix matrix3 = new(matrixItems3);
+            Single[,] matrixItems4 = new Single[,] { { 1 }, { 2 }, { 3 } };
+            Matrix matrix4 = new(matrixItems4);
+
+            Matrix result2 = matrix3.Multiply(matrix4);
+
+            Assert.Equal(2, result2.GetDimension0());
+            Assert.Equal(1, result2.GetDimension1());
+            Assert.Equal(10f, result2.GetItem(0, 0));
+            Assert.Equal(8f, result2.GetItem(1, 0));
         }
 
     }
--- src/Math/Matrix.cs
+++ src/Math/Matrix.cs
@@ -49,11 +49,28 @@ namespace Math
         public Matrix Multiply(Single scalar)
         {
             Single[,] newMatrix = new Single[GetDimension0(), GetDimension1()];
-            for (Int32 i=0; i<GetDimension0(); i++)
+            for (Int32 i = 0; i < GetDimension0(); i++)
             {
-                for (Int32 k=0; k<GetDimension1(); k++)
+                for (Int32 k = 0; k < GetDimension1(); k++)
                 {
                     newMatrix[i, k] = GetItem(i, k) * scalar;
+                }
+            }
+            Matrix result = new(newMatrix);
+            return result;
+        }
+
+        public Matrix Multiply(Matrix matrix)
+        {
+            Single[,] newMatrix = new Single[GetDimension0(), matrix.GetDimension1()];
+            for (int i = 0; i < GetDimension0(); i++)
+            {
+                for (int k = 0; k < matrix.GetDimension1(); k++)
+                {
+                    for (Int32 m = 0; m < GetDimension1(); m++)
+                    {
+                        newMatrix[i, k] += GetItem(i, m) * matrix.GetItem(m, k);
+                    }
                 }
             }
             Matrix result = new(newMatrix);
