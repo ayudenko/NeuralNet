3b6d0060dc18b2ef4e85a14f967e16f4ea26f0d6
Andrey Yudenko <ayudenko@gmail.com>
2021-03-05 22:17:19 +0300

    Add RELU activation function

---
+++ src/Models.Test/NeuralNetModels/ActivationFunctions/RELUTests.cs
@@ -0,0 +1,23 @@
+﻿using Models.NeuralNetModels.ActivationFunctions;
+using Xunit;
+
+namespace Models.Test.NeuralNetModels.ActivationFunctions
+{
+    public class RELUTests
+    {
+
+        [Theory]
+        [InlineData(3.2f, 3.2f)]
+        [InlineData(0f, 0f)]
+        [InlineData(-1.1f, 0f)]
+        public void Execute_ShouldReturnMaxValeFromZeroAndGivenValue(float weightedSum, float result)
+        { 
+            IActivationFunction function = new RELU();
+
+            float output = function.Execute(weightedSum);
+
+            Assert.Equal(result, output);
+        }
+
+    }
+}
---
+++ src/Models/NeuralNetModels/ActivationFunctions/RELU.cs
@@ -0,0 +1,10 @@
+﻿namespace Models.NeuralNetModels.ActivationFunctions
+{
+    public class RELU : IActivationFunction
+    {
+        public float Execute(float weightedSum)
+        {
+            return System.Math.Max(0, weightedSum);
+        }
+    }
+}
